bf65c997dcb6d64a3850a4a36b146cb424f38cdf
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-19 19:24:31 +0530

    Update renderer.js

--- src/app/pages/home-page/renderer.js
+++ src/app/pages/home-page/renderer.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var canvas = document.getElementsByClassName('visualizer')[0];//querySelector('.visualizer');
+  var canvas = document.getElementsById('canvasvisualizer');//querySelector('.visualizer');
   var canvasCtx = canvas.getContext('2d');
   var listening = true;
 
