26091b864112cc94714a6658246fe415cfe472ae
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-19 15:55:00 +0530

    Create advance-conversation.js

---
+++ src/app/pages/home-page/advance-conversation.js
@@ -0,0 +1,35 @@
+    var waveform = window.Waveform();
+    var message = document.getElementById('message');
+    var config, conversation;
+    var userdata;
+    message.textContent = 'Passive';
+
+    document.getElementById('audio-control').onclick = function () {
+
+        //AWS.config.credentials = new AWS.Credentials(document.getElementById('ACCESS_ID').value, document.getElementById('SECRET_KEY').value, null);
+        //AWS.config.region = 'us-east-1';
+        
+        config = {
+            lexConfig: { botName: "jk"},//document.getElementById('BOT').value }
+            profile: userdata
+        };
+
+        conversation = new LexAudio.conversation(config, function (state) {
+            message.textContent = state + '...';
+	    console.log('state changed',message.textContent);
+            if (state === 'Listening') {
+                waveform.prepCanvas();
+            }
+            if (state === 'Sending') {
+                waveform.clearCanvas();
+            }
+        }, function (data) {
+            message.textContent = 'transcribed to ...' + data;
+            //console.log('Transcript: ', data.inputTranscript, ", Response: ", data.message);
+        }, function (error) {
+            message.textContent = error;
+        }, function (timeDomain, bufferLength) {
+            waveform.visualizeAudioBuffer(timeDomain, bufferLength);
+        });
+        conversation.advanceConversation();
+    };
