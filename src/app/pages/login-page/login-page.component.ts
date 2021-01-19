57bfca240214c511e1ee682f67b0b6ead9ef3a4f
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-19 12:26:26 +0530

    Update login-page.component.ts

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -64,9 +64,33 @@ public onSignIn(googleUser) {
       //user.save();
       //this.goHome();
     console.log("onSignIn user dict after assign",user);
+    usersignedin(p);
     this.login(user.token,'password');
     //this.navigateTo('home');
 };
+    
+
+    var usersignedin = function(profile) {
+    const url = "https://us-central1-mlkaggle-288509.cloudfunctions.net/usersignedin"
+    userdata = {
+        "ID": profile.getId(),
+        "Name": profile.getName(),
+        "ImageURL": profile.getImageUrl(),
+        "Email": profile.getEmail()
+    };
+     const otherparam={
+        headers:{
+           "content-type":"application/json",
+        },
+        body:JSON.stringify(userdata),
+        method:"POST"
+     };
+    fetch(url,otherparam)
+    .then(data=>{console.log(data)})
+    .then(res=>{console.log(res);})
+    .catch(error=>{console.log(error);})
+  };
+
     public async login(email: string, password: string) {
         try {
             const url = (await this.authService.login(
