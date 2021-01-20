8fc6d1e16e8d24574bf784476377137771a811fa
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-20 07:43:42 +0530

    Update advance-conversation.js

--- advance-conversation.js
+++ advance-conversation.js
@@ -1,11 +1,11 @@
+
+    export default function js_advanceconversation() {
     var waveform = window.Waveform();
     var message = document.getElementById('message');
     var config, conversation;
     var userdata;
     message.textContent = 'Passive';
     console.log("inside advance conversation js");
-    export default function js_advanceconversation() {
-
         //AWS.config.credentials = new AWS.Credentials(document.getElementById('ACCESS_ID').value, document.getElementById('SECRET_KEY').value, null);
         //AWS.config.region = 'us-east-1';
         
