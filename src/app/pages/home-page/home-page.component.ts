e670b15274d99505e43689aa53eae08012c2af36
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-20 06:43:08 +0530

    Update home-page.component.ts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -16,31 +16,30 @@ export class HomePageComponent implements OnInit {
     }
     
        ngAfterViewInit() {
-           /*
+           
      var s = document.createElement("script");
      s.type = "text/javascript";
-     s.src = "src/app/pages/home-page/jsrsasign-all-min.js";
+     s.src = "https://jkreddy123.github.io/checktablesweb/jsrsasign-all-min.js";
      document.body.appendChild(s);
      
           var s1 = document.createElement("script");
      s1.type = "text/javascript";
-     s1.src = "app/pages/home-page/constants.js";
+     s1.src = "https://jkreddy123.github.io/checktablesweb/constants.js";
      document.body.appendChild(s1);
      
           var s2 = document.createElement("script");
      s2.type = "text/javascript";
-     s2.src = "pages/login-page/GCPuserLoginfunctionAPI.js";
+     s2.src = "https://jkreddy123.github.io/checktablesweb/GCPuserLoginfunctionAPI.js";
      document.body.appendChild(s2);
      
           var s3 = document.createElement("script");
-     s3.type = "text/javascript";
-     s3.src = "home-page/renderer.js";
+     s3.typhttps://jkreddy123.github.io/checktablesweb/home-page/renderer.js";
      document.body.appendChild(s3);
      
      
           var s4 = document.createElement("script");
      s4.type = "text/javascript";
-     s4.src = "aws-lex-audio.js";
+     s4.src = "https://jkreddy123.github.io/checktablesweb/aws-lex-audio.js";
      document.body.appendChild(s4);
      
      
@@ -48,8 +47,8 @@ export class HomePageComponent implements OnInit {
      
           var s5 = document.createElement("script");
      s5.type = "text/javascript";
-     s5.src = "./advance-conversation.js";
+     s5.src = "https://jkreddy123.github.io/checktablesweb/advance-conversation.js";
      document.body.appendChild(s5);
-     */
+     
    }
 }
