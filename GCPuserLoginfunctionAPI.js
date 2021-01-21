053dc1ebce773d590fac8143020b6c7f418c7cb5
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-21 06:41:34 +0530

    Update GCPuserLoginfunctionAPI.js

--- GCPuserLoginfunctionAPI.js
+++ GCPuserLoginfunctionAPI.js
@@ -18,3 +18,21 @@ function usersignedin(profile) {
     .then(res=>{console.log(res);})
     .catch(error=>{console.log(error);})
   };
+  
+  function tableconversations(profile) {
+    const url = "https://us-central1-mlkaggle-288509.cloudfunctions.net/tableconversations"
+    userdata = {
+        "ID": profile.getId(),
+    };
+     const otherparam={
+        headers:{
+           "content-type":"application/json",
+        },
+        body:JSON.stringify(userdata),
+        method:"POST"
+     };
+    fetch(url,otherparam)
+    .then(data=>{console.log(data)})
+    .then(res=>{console.log(res);})
+    .catch(error=>{console.log(error);})
+  };
