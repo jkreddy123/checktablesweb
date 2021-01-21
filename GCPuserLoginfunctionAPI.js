2763f0b149e0cb0d1aeae62444fdc8bee9e2bf0a
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-21 10:48:54 +0530

    Update GCPuserLoginfunctionAPI.js

--- GCPuserLoginfunctionAPI.js
+++ GCPuserLoginfunctionAPI.js
@@ -32,7 +32,7 @@ function usersignedin(profile) {
         method:"POST"
      };
     fetch(url,otherparam)
-    .then(data=>{console.log(data); return JSON.parse(data)})
+    .then(data=>{console.log(data); return data.json()})
     .then(res=>{console.log(res);return res;})
     .catch(error=>{console.log(error);})
   };
