84ee95d5e716aed827ecdce39cbb18d6cfcde239
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-22 22:35:39 +0530

    Update logo.component.ts

--- src/app/core/components/logo/logo.component.ts
+++ src/app/core/components/logo/logo.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit,ChangeDetectorRef, AfterContentChecked} from '@angular/core';
 
 @Component({
     selector: 'app-logo',
     templateUrl: './logo.component.html',
     styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent implements OnInit {
-    constructor() {}
+export class LogoComponent implements OnInit,AfterViewInit {
+    currentphoto:string = '';
+    
+    constructor(private cdRef: ChangeDetectorRef) {}
 
     ngOnInit() {}
+    ngAfterViewInit() {
+
+        var userimageurl = localStorage.getItem("userimageurl");
+        console.log("in logo component",userimageurl);
+        this.currentphoto = userimageurl;
+        //document.getElementById('imageDiv').innerHTML = '<img src="' + userimageurl + '" class="rounded-corners"/>';
+        this.cdRef.detectChanges();
+    }
+
 }
