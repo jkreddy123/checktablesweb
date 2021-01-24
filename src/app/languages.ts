c329bcfe8e5940a91df7cdc5c49ce60d2a68f250
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-24 18:03:47 +0530

    Update languages.ts

--- src/app/languages.ts
+++ src/app/languages.ts
@@ -1,9 +1,11 @@
 const languages = [
+{ name: "English (India)	",  code:"en-IN"},	
+{ name: "English (United Kingdom) ",  code:"en-GB"},
+{ name: "English (United States)",  code:"en-US"},
 { name: "English (Australia)	",  code:"en-AU"},
 { name: "English (Canada)	",  code:"en-CA"},
 { name: "English (Ghana)     ",  code:"en-GH"},	
 { name: "English (Hong Kong)	",  code:"en-HK"},	
-{ name: "English (India)	",  code:"en-IN"},	
 { name: "English (Ireland)	",  code:"en-IE"},	
 { name: "English (Kenya)	",  code:"en-KE"},
 { name: "English (New Zealand)	",  code:"en-NZ"},	
@@ -13,7 +15,5 @@ const languages = [
 { name: "English (Singapore)	",  code:"en-SG"},
 { name: "English (South Africa)	",  code:"en-ZA"},
 { name: "English (Tanzania)	",  code:"en-TZ"},
-{ name: "English (United Kingdom) ",  code:"en-GB"},
-{ name: "English (United States)",  code:"en-US"},
 ];
 export default languages
