ad4df577441b7543dc8d953ca54093059824ef31
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-24 22:41:01 +0530

    Update aws-lex-audio.js

--- aws-lex-audio.js
+++ aws-lex-audio.js
@@ -247,7 +247,7 @@
     SENDING: 'Sending',
     SPEAKING: 'Speaking'
   });
-
+  var LANG_CODE =  localStorage.getItem("englishaccent");
   var lexruntime, audioControl = new AudioControl({ checkAudioSupport: false });
 
   exports.conversation = function(config, onStateChange, onSuccess, onError, onAudioData) {
@@ -376,7 +376,7 @@
     const url = 'https://speech.googleapis.com/v1p1beta1/speech:recognize?key='+API_KEY
     const data = {
       "config": {
-         "languageCode": "en-IN",
+         "languageCode": LANG_CODE,
          "maxAlternatives":1,
          "profanityFilter":true,
          "enableWordTimeOffsets": false
@@ -423,7 +423,7 @@
          'text':text_msg
       },
       'voice':{
-         'languageCode':'en-IN',
+         'languageCode':LANG_CODE,
          'name':'en-GB-Standard-A',
          'ssmlGender':'FEMALE'
       },
@@ -475,7 +475,7 @@ function playOutput(base64_string){
     const data = {
         "queryInput": {
            "text": {"text": text_msg,
-                    "languageCode": 'en-IN' 
+                    "languageCode": LANG_CODE 
                    }
         },
        "queryParams":{
@@ -546,105 +546,7 @@ function playOutput(base64_string){
         tip.textContent = 'say \"20\" OR \"i dont know\" OR \"skip\" OR \"start over\"';
     }               
   }
-function postJWT(jwt, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200 && callback) {
-                callback(this.responseText);
-                console.log(this.responseText);
-                return;
-            }
-            if (console) console.log(this.responseText);
-        }
-    };
-    var parameters = "grant_type=" + encodeURIComponent("urn:ietf:params:oauth:grant-type:jwt-bearer") + "&assertion=" + encodeURIComponent(jwt);
-    xhttp.open("POST", "https://www.googleapis.com/oauth2/v4/token", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(parameters);
-}
 
-function getCert() {
-    var cert = //your json key (downloaded earlier) goes here
-     {
-        "type": "service_account",
-  	"project_id": PROJECT_ID,
-  	"private_key_id": PRIVATE_KEY_ID,
-  	"private_key": PRIVATE_KEY,
-  	"client_email": CLIENT_EMAIL,
-  	"client_id": CLIENT_ID,
-  	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  	"token_uri": "https://oauth2.googleapis.com/token",
-  	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  	"client_x509_cert_url": CLIENT_X509_CERT_URL
-     };      
-    return cert;
-}
-function getJWT() {
-
-    var imported = document.createElement('script');
-    imported.src = 'jsrsasign-all-min.js';
-    document.head.appendChild(imported);
-
-    var cert = getCert();
-    var key = KEYUTIL.getKey(cert.private_key);
-    var headers = { "alg": "RS256", "typ": "JWT" };
-    var issued = Math.floor(new Date().getTime()/1000);
-
-    var claims = {
-        "iss": cert.client_email,
-        "scope": "https://www.googleapis.com/auth/dialogflow",
-        "aud": "https://www.googleapis.com/oauth2/v4/token",
-        "exp": issued + 3600,
-        "iat": issued
-    };
-
-    var jwt = KJUR.jws.JWS.sign(headers.alg, headers, JSON.stringify(claims), key);
-    return jwt;
-}
-
-function jsonToStructProto(json) {
-  const fields = {};
-  for (let k in json) {
-    fields[k] = jsonValueToProto(json[k]);
-  }
-
-  return {fields};
-}
-
-const JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP = {
-  [typeof 0]: 'numberValue',
-  [typeof '']: 'stringValue',
-  [typeof false]: 'boolValue',
-};
-
-const JSON_SIMPLE_VALUE_KINDS = new Set([
-  'numberValue',
-  'stringValue',
-  'boolValue',
-]);
-
-function jsonValueToProto(value) {
-  const valueProto = {};
-
-  if (value === null) {
-    valueProto.kind = 'nullValue';
-    valueProto.nullValue = 'NULL_VALUE';
-  } else if (value instanceof Array) {
-    valueProto.kind = 'listValue';
-    valueProto.listValue = {values: value.map(jsonValueToProto)};
-  } else if (typeof value === 'object') {
-    valueProto.kind = 'structValue';
-    valueProto.structValue = jsonToStructProto(value);
-  } else if (typeof value in JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP) {
-    const kind = JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP[typeof value];
-    valueProto.kind = kind;
-    valueProto[kind] = value;
-  } else {
-    console.warn('Unsupported value type ', typeof value);
-  }
-  return valueProto;
-}
 
   var Sending = function(state) {
     this.state = state;
