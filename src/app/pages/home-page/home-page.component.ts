b2ee856b8fe4a7bc64542e98b81d89e18bed323e
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-02-04 11:19:43 +0530

    Update home-page.component.ts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,7 +4,10 @@ import { Inject } from '@angular/core';
 import { UserobjectserviceService } from "src/app/userobjectservice.service";
 declare function js_advanceconversation(profile: any): any;
 declare function stopRecord_click(): any;
-
+declare function usersignedin(): any;
+declare function  get_JWT_token(): any;
+declare function  loadscripts(): void;
+//declare function  loadscripts(profile: object | null, user:object|null): void;
 
 @Component({
     selector: 'app-home-page',
@@ -14,7 +17,9 @@ declare function stopRecord_click(): any;
 export class HomePageComponent implements OnInit {
     constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef,private UserobjectserviceService: UserobjectserviceService) {}
 
-    ngOnInit() {}
+    ngOnInit() {    
+    	this.loadscripts();
+    }
     
     public async advanceconversation() {
         console.log("advanceconversation click");
@@ -26,6 +31,39 @@ export class HomePageComponent implements OnInit {
       stopRecord_click();
     }
     
+   public loadscripts(){
+       var s0 = document.createElement('script');
+        s0.type = "text/javascript";
+    	s0.src = "./assets/js/jsrsasign-all-min.js"; 
+    	s0.defer = true; 
+    	document.body.appendChild(s0);
+    	s0.onload = () => {
+	     	var s1 = document.createElement('script');
+    		s1.src = "./assets/js/constants.js"; 
+   	 	//s1.defer = true; 
+  	  	document.body.appendChild(s1);
+    	
+    		var s2 = document.createElement('script');
+   	 	s2.src = "./assets/js/jwttoken.js"; 
+   	 	s2.defer = true; 
+  	  	document.body.appendChild(s2);
+  	  	s2.onload = () => {
+        		get_JWT_token();//this will get 1 hour valid dialogflow token and set in localstorage
+        		var s3 = document.createElement("script");
+    			 s3.type = "text/javascript";
+   			 s3.src = "./assets/js/GCPuserLoginfunctionAPI.js";
+   			 s3.defer=true;
+   			 //this.elementRef.nativeElement.appendChild(s3);
+    			 document.body.appendChild(s3);
+    			 s3.onload = () => {
+     	  			  usersignedin();
+     	  			  //this.login(user.token,user.ID);//'password');
+   			  }
+            	}   	
+
+        }
+
+}
 
   
      ngAfterViewInit() {
@@ -54,6 +92,7 @@ export class HomePageComponent implements OnInit {
           var s5 = document.createElement("script");
      s5.type = "text/javascript";
      s5.src = "./assets/js/advance-conversation.js";
+     s5.async = true;
      document.body.appendChild(s5);
      
      
